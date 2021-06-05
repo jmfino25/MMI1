c046017d52e16f08d5877c399eb82ca99e70574f
Jean-Marie Fino <68325240+jmfino25@users.noreply.github.com>
2021-06-05 07:43:22 +0200

    Add files via upload

---
+++ lesgaulois02/pages/accueil.vue.js
@@ -0,0 +1,17 @@
+var Accueil = Vue.component('Accueil',{
+    template:`
+    <div>
+        <img src="images/bagarreGauloise.jpg"  alt="les gaulois" class="img-fluid"/>
+    </div>
+    `,
+    data(){
+        return{
+        }
+    },
+    mounted(){
+        
+    },
+    methods:{
+
+    }
+})
